c705647b1e9501d58434c2adea2ee26a2eb9eba9
k-sapp <kalenasapp@gmail.com>
2026-04-28 08:32:06 -0700

    #script

---
+++ Week_3/script_notes_week_3.R
@@ -0,0 +1,11 @@
+#### Week 3 notes 
+quick = 1
+maths = 3
+da_ting = quick + maths 
+da_ting
+
+#conflict "\n" <- for working with strings
+#ammend previous commit: change last commit with new changes, updates history 
+#to only include new changes
+#branching allows you to create seperate version of code without affecting main version
+#can later merge branch into main
